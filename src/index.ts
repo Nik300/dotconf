be9ef99542dc9d56867525bb4e67d91e6402bc7b
Nicola Leone Ciardi (@Falco131) <nicolaleone.ciardi@gmail.com>
2022-11-20 18:33:59 +0100

    published to npmjs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { ParseString } from "./parser";
 import { readFileSync } from 'fs';
 
-process.env = { ...process.env, ...ParseString(readFileSync("./.conf", { encoding: "utf-8" })) };
+process.env = { ...process.env, ...ParseString(readFileSync("./.env", { encoding: "utf-8" })) };
 
 export default process.env;
